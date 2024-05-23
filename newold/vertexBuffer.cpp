30f4f94c85b359bc841eb782e14d89e55f16fa07
Niko Korkala <nikokorkala5@kamk.fi>
2024-05-23 18:27:44 +0300

    delete bunch of things

--- newold/vertexBuffer.cpp
+++
@@ -1,6 +0,0 @@
-#include "vertexBuffer.h"
-
-namespace kge
-{
-
-}
--- shaders/compile.bat
+++
@@ -1,2 +0,0 @@
-C:/VulkanSDK/1.3.239.0/Bin/glslc.exe VertexShader.vert -o vert.spv
-C:/VulkanSDK/1.3.239.0/Bin/glslc.exe FragmentShader.frag -o frag.spv
